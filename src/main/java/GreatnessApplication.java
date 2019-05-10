d23b85ed9274ffab274f949b0c04da4fb8b773b5
madathil22 <tobin22@gmail.com>
2019-05-10 17:41:00 -0400

    first commit

---
+++ src/main/java/GreatnessApplication.java
@@ -0,0 +1,13 @@
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreatnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreatnessApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/greatness/config/AppConfig.java
@@ -0,0 +1,8 @@
+package com.greatness.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+}
