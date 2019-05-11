e56c92a4ef7b545c462b8fb9684851dbc7bf7032
madathil22 <tobin22@gmail.com>
2019-05-11 13:14:35 -0400

    changes

--- src/main/java/com/greatness/config/AppConfig.java
+++ src/main/java/com/greatness/config/AppConfig.java
@@ -1,8 +1,38 @@
 package com.greatness.config;
 
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class AppConfig {
 
+	
+
+	@Value("${spring.datasource.username}")
+	private String username;
+	
+	@Value("${spring.datasource.password}")
+	private String password;
+	
+	@Value("${spring.datasource.url}")
+	private String url;
+	
+	@Bean
+	@Primary
+	DataSource dataSource() throws SQLException{
+		return DataSourceBuilder
+		.create()
+		.url(url)
+		.username(username)
+		.password(password)
+		.build();		
+	}
+	
 }
